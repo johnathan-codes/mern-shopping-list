7ece959b2886240de155a6892a183c4fd8096855
Andrej Šebeň <andrej.seben@millennium.sk>
2019-05-21 13:14:13 +0200

    Added client side
    Created client side via create-react-app.
    Added few scripct into root/package.json.

--- models/Item.js
+++ models/Item.js
@@ -3,17 +3,17 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const ItemSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 },
 {
-    collection: 'shopping-list'
+  collection: 'shopping-list'
 });
 
-module.exports = Item = mongoose.model('item',ItemSchema);
+module.exports = Item = mongoose.model('item', ItemSchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -13,7 +14,7 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log('Database connection successful.'))
     .catch(err => console.log(err));
 
@@ -21,4 +22,4 @@ app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
