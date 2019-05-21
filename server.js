1afdf3fa0139bb91e21cc80e95b3367ceb22ee47
Andrej Šebeň <andrej.seben@millennium.sk>
2019-05-21 09:43:08 +0200

    MongoDB and server
    Created Item model.
    Created connection to MongoDB.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose.connect(db)
+    .then(() => console.log('Database connection successful.'))
+    .catch(err => console.log(err));
+
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
