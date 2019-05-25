b73e75a4c526d12eea4c8f7f1ec126333cb6de12
Andrej Šebeň <andrej.seben@millennium.sk>
2019-05-25 12:38:04 +0200

    Deploying to Heroku
    Renamed app (the browser tab).
    Added postbuild script.
    Added production mode in server.js

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ mongoose.connect(db, { useNewUrlParser: true })
 
 app.use('/api/items', items);
 
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
