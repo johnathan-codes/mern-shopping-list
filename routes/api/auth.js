6f76879c86278f5cbeebf9f13226f665f6ff14a0
Andrej Šebeň <andrej.seben@millennium.sk>
2019-05-30 18:56:55 +0200

    JWT Auth
    Added register, login, logout.
    Changed icon.
    Added authorization actions.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,53 +12,51 @@ const User = require('../../models/User');
 // @desc    Auth user
 // @access  Public
 router.post('/', (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if( !email || !password){
-        return res.status(400).json({msg: 'Please enter all fields'});
+  if (!email || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(400).json({ msg: 'User does not exists' });
     }
 
-    User.findOne({email})
-        .then(user => {
-            if(!user){
-                return res.status(400).json({ msg: 'User does not exists' })
-            };
+    //validate password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid email or password!' });
+      }
 
-            //validate password
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if(!isMatch){
-                        return res.status(400).json({ msg: 'Invalid email or password!'});
-                    }
-
-                    jwt.sign(
-                        { id: user.id }, 
-                        config.get('jwtSecret'), 
-                        { expiresIn: 3600 }, //expires in seconds
-                        (err, token) => {
-                            if(err){
-                                throw err
-                            };
-                            res.json({
-                                token,
-                                user: {
-                                    id: user.id,
-                                    name: user.name,
-                                    email: user.email
-                                }
-                            });
-                        }
-                    );
-                })
-        })
+      jwt.sign(
+        { id: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: 3600 }, //expires in seconds
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({
+            token,
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email
+            }
+          });
+        }
+      );
+    });
+  });
 });
 
 // @route   GET api/auth/user
 // @desc    Get user data
 // @access  Private
 router.get('/user', auth, (req, res) => {
-    User.findById(req.user.id)
-        .select('-password')
-        .then(user => res.json(user));
-})
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
+});
 module.exports = router;
