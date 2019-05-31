4d43369d6ffe4840ee757f1ed5aad5056eb503d5
Andrej Šebeň <andrej.seben@millennium.sk>
2019-05-31 10:23:38 +0200

    Finishing - hopefully

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,14 @@
-const config = require('config');
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
   const token = req.header('x-auth-token');
 
-  //check for token
   if (!token) {
     return res.status(401).json({ msg: 'Unauthorized access' });
   }
 
   try {
-    //verify token
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-    //Add user from payload
+    const decoded = jwt.verify(token, process.env.JWT);
     req.user = decoded;
     next();
   } catch (error) {
